6e0c1c086fb3bc73f8a2373053ab675c3096c7f7
Tom Lovett <tomlovett6@gmail.com>
2018-01-11 11:10:06 -0500

    Connect components to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Upload from './components/Upload';
+import Display from './components/Display';
+import Callback from './components/Callback';
+// import registerServiceWorker from './registerServiceWorker';
+import { Router, Route, browserHistory } from 'react-router';
+import { requireAuth } from './utils/AuthService';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const Root = () => {
+  return (
+    <div className="container">
+      <Router history={browserHistory}>
+        <Route path="/" component={Display}/>
+        <Route path="/upload" component={Upload} onEnter={requireAuth} />
+        <Route path="/callback" component={Callback} />
+      </Router>
+    </div>
+  )
+}
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+// registerServiceWorker();
