2ca671119fbe35af0ae4bf7a02f4ccd645028d18
Tom Lovett <tomlovett6@gmail.com>
2018-01-12 09:58:25 -0500

    Display videos in Display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Nav from './Nav';
 import { isLoggedIn } from '../utils/AuthService';
+import { CloudinaryContext, Transformation, Video } from 'cloudinary-react';
 import axios from 'axios';
 
 class Display extends Component {
 
+  state = { videos: [] };
+
+  getVideos() {
+    axios.get('http://res.cloudinary.com/unicodeveloper/video/list/miniflix.json')
+      .then(res => {
+        this.setState({ videos: res.data.resources});
+      });
+  }
+
+  componentDidMount() {
+    this.getVideos();
+  }
+
   render() {
+
+    const { videos } = this.state;
+    console.log('videos.length: ', videos.length);
 
     return (
       <div>
@@ -15,7 +32,17 @@ class Display extends Component {
         <hr />
 
         <div className="col-sm-12">
-
+          <CloudinaryContext cloudName="unicodeveloper">
+            { videos.map((data, index) => (
+                <div className="col-sm-4" key={index}>
+                  <div className="embed-responsive embed-responsive-4by3">
+                    <Video publicId={data.public_id} width="300" height="300" controls></Video>
+                  </div>
+                  <div> Created at {data.created_at} </div>
+                </div>
+              ))
+            }
+          </CloudinaryContext>
         </div>
       </div>
     );
